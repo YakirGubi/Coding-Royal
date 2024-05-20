6bd575b1445171a58872d69cec112989e3f14e19
YairYaad <yair.yaad.ben.shimol@gmail.com>
2024-05-20 19:18:45 +0300

    basic tankg and tanks
    basic tankg and tanks

--- src/TankS.java
+++ src/TankS.java
@@ -1,2 +1,40 @@
-public class TankS {
+public class TankS extends TankG{
+
+    public TankS(double[] locationV, double[] directionV) {
+        this.locationV = locationV;
+        this.directionV = directionV;
+        this.HP = this.maxHP;
+        this.bullet_amount = this.magazine_capacity;
+        this.speed = 0;
+        this.time_in_reload = 0;
+        this.is_reload = false;
+    }
+
+    public void setLocationV(double[] locationV) {
+        this.locationV = locationV;
+    }
+
+    public void setDirectionV(double[] directionV) {
+        this.directionV = directionV;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public void setBullet_amount(int bullet_amount) {
+        this.bullet_amount = bullet_amount;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setTime_in_reload(double time_in_reload) {
+        this.time_in_reload = time_in_reload;
+    }
+
+    public void setIs_reload(boolean is_reload) {
+        if (!this.is_reload) this.is_reload = is_reload;
+    }
 }
